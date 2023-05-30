96205180536480a7a72f3afd79f9f8428a3ff000
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-05-30 14:51:21 -0500

    Added delete and edit users

---
+++ src/main/java/com/codeup/adlister/controllers/EditUserServlets.java
@@ -0,0 +1,55 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+import com.codeup.adlister.util.Password;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "controllers.EditUserServlets", urlPatterns = "/editUser")
+public class EditUserServlets extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getSession().getAttribute("user") == null) {
+            resp.sendRedirect("/login");
+            return;
+        }
+        req.getRequestDispatcher("/WEB-INF/edit.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String newUsername = req.getParameter("newUsername");
+        String newEmail = req.getParameter("newEmail");
+        String newPassword = req.getParameter("newPassword");
+        String newPasswordConfirmation = req.getParameter("newConfirm_password");
+
+        /* validate input */
+        boolean inputHasErrors = newUsername.isEmpty()
+                || newEmail.isEmpty()
+                || newPassword.isEmpty()
+                || (! newPassword.equals(newPasswordConfirmation));
+
+        if (inputHasErrors) {
+            resp.sendRedirect("/editUser");
+            return;
+        }
+
+        // create and save a new user
+        User user = new User(newUsername, newEmail, newPassword);
+
+        // hash the password
+
+        String hash = Password.hash(user.getPassword());
+
+        user.setPassword(hash);
+
+        DaoFactory.getUsersDao().insert(user);
+        resp.sendRedirect("/login");
+    }
+}
