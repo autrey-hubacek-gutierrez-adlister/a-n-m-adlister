51858fa9056ba286626cedfd587cb46c2b022d2a
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-06-01 16:28:57 -0500

    Added detail serlvet

--- src/main/java/com/codeup/adlister/controllers/AdsDetailServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdsDetailServlet.java
@@ -1,5 +1,6 @@
 package com.codeup.adlister.controllers;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +12,11 @@ import java.io.IOException;
 public class AdsDetailServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String adId = req.getParameter("id");
 
+        req.setAttribute("adTitle", "Sample Ad");
+        req.setAttribute("adDescription", "This is a sample ad description.");
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/ad_details.jsp");
+        dispatcher.forward(req, resp);
     }
 }
